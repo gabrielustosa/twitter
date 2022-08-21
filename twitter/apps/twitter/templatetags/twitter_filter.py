3cdcefe2c91afc959f7c59309739b78040a8199b
Gabriel Lustosa Queiroz <lustosaki2@gmail.com>
2022-08-21 13:36:12 -0300

    FEAT[TWITTER]: parse tweet e arrumando templates

--- twitter/apps/twitter/templatetags/twitter_filter.py
+++ twitter/apps/twitter/templatetags/twitter_filter.py
@@ -41,3 +41,34 @@ def format_tweet_numbers(value):
         return f'{value / 1000:.1f}K'
 
     return value
+
+
+@register.filter()
+def parse_tweet(tweet):
+    words = [word for word in tweet.split() if word.startswith('#') or word.startswith('@')]
+
+    parsed_tweet = ''
+    for index, word_twitter in enumerate(tweet.split()):
+        new_word = tweet.split()[index]
+        for word in words:
+            if word == word_twitter:
+                wrapped_word = f'<a href="/teste" class="text-blue-400 hover:underline">{word}</a>'
+                new_word = wrapped_word
+        parsed_tweet += f'{new_word} '
+
+    return parsed_tweet
+
+
+@register.filter()
+def tweet_ancestors_parsed(tweet):
+    ancestors = tweet.get_ancestors(include_self=False)
+
+    two = ancestors[0:2]
+
+    string_format = '<a href="/teste" class="text-blue-400 hover:underline">@{0}</a>'
+    ancestors_parsed = ' '.join([string_format.format(tweet.creator.name) for tweet in two])
+
+    if ancestors.count() > 2:
+        ancestors_parsed += f' <span class="text-blue-400 hover:underline">e mais {ancestors.count()}</span>'
+
+    return ancestors_parsed
